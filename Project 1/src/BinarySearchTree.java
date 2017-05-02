1cec06118762aa779c737383201250a15c25f453
ohheythereandy <andyjrosas@gmail.com>
2017-05-01 20:16:14 -0700

    Implemented findPredecessor and findSuccessor, need test cases.

--- Project 1/src/BinarySearchTree.java
+++ Project 1/src/BinarySearchTree.java
@@ -1,5 +1,3 @@
-import java.util.Iterator;
-
 /**
  * Created by Andy on 4/26/17.
  */
@@ -9,12 +7,8 @@ public class BinarySearchTree<T extends Comparable
     private BinaryNodeInterface<T> root;
 
     public BinarySearchTree(){
-        this(null);
+        root=null;
     }
-
-    public BinarySearchTree(T rootEntry){
-        setRootNode(new BinaryNode<T>(rootEntry));
-    }//end constructor
 
     protected void setRootNode(BinaryNodeInterface<T> rootNode){
         root = rootNode;
@@ -36,12 +30,12 @@ public class BinarySearchTree<T extends Comparable
      @return either the object that was found in the tree or
      null if no such object exists */
     public T getEntry(T entry){
-        return findEntry(getRootNode(), entry);
+        return findEntry(getRootNode(), entry).getData();
     }
 
 
-    private T findEntry(BinaryNodeInterface<T> rootNode, T anEntry){
-        T result = null;
+    private BinaryNodeInterface<T> findEntry(BinaryNodeInterface<T> rootNode, T anEntry){
+        BinaryNodeInterface<T> result = new BinaryNode<>(null);
 
         //if rootNode exists(Valid Tree)
         if(rootNode != null)
@@ -49,7 +43,7 @@ public class BinarySearchTree<T extends Comparable
             T rootEntry = rootNode.getData();
             //if given Entry equals root, returns
             if(anEntry.equals(rootEntry))
-                result = rootEntry;
+                result = rootNode;
             //if given Entry is less than root, go left
             else if(anEntry.compareTo(rootEntry)<0)
                 result = findEntry(rootNode.getLeftChild(), anEntry);
@@ -71,7 +65,7 @@ public class BinarySearchTree<T extends Comparable
         T result = null;
 
         if(isEmpty())
-            setRootNode(new BinaryNode<T>(newEntry));
+            setRootNode(new BinaryNode<>(newEntry));
         else
             result = addEntry(getRootNode(), newEntry);
         return result;
@@ -191,14 +185,15 @@ public class BinarySearchTree<T extends Comparable
     public void printInOrder(BinaryNodeInterface<T> root){
         if(root!= null){
             printInOrder(root.getLeftChild());
-            System.out.println(root.getData());
+            System.out.print(root.getData() + " ");
             printInOrder(root.getRightChild());
+
         }
     }
 
     public void printPreOrder(BinaryNodeInterface<T> root){
         if(root!=null) {
-            System.out.println(root.getData());
+            System.out.print(root.getData() + " ");
             printPreOrder(root.getLeftChild());
             printPreOrder(root.getRightChild());
         }
@@ -208,8 +203,74 @@ public class BinarySearchTree<T extends Comparable
         if(root!=null){
             printPostOrder(root.getLeftChild());
             printPostOrder(root.getRightChild());
-            System.out.println(root.getData());
+            System.out.print(root.getData() + " ");
         }
+    }
+
+    public void findSuccessor(BinaryNodeInterface<T> root ,T anEntry) {
+        BinaryNodeInterface<T> anEntryNode = findEntry(root, anEntry);
+        BinaryNodeInterface<T> succ = new BinaryNode<>(null);
+        if (anEntryNode.getData() != null) {
+            if (anEntryNode.hasRightChild()) {
+                minValue(anEntryNode.getRightChild());
+                return;
+            }
+            while (root.getData() != null) {
+                if (anEntryNode.getData().compareTo(root.getData()) < 0) {
+                    succ = root;
+                    root = root.getLeftChild();
+                } else if (anEntryNode.getData().compareTo(root.getData()) > 0) {
+                    root = root.getRightChild();
+                } else
+                    break;
+            }
+            System.out.println(succ.getData());
+        }
+        else{
+                System.out.println("Sorry, that node doesn't exist!");
+            }
+
+    }
+
+    public void findPredecessor(BinaryNodeInterface<T> root, T anEntry){
+        BinaryNodeInterface<T> anEntryNode = findEntry(root, anEntry);
+        BinaryNodeInterface<T> pred = new BinaryNode<>(null);
+        if(anEntryNode.getData()!= null){
+            if(anEntryNode.hasLeftChild()){
+                maxValue(anEntryNode.getRightChild());
+            }
+            while(root.getData() != null){
+                if(anEntryNode.getData().compareTo(root.getData()) > 0){
+                    pred = root;
+                    root = root.getRightChild();
+                }
+                else if( anEntryNode.getData().compareTo(root.getData()) < 0){
+                    root = root.getLeftChild();
+                }
+            }
+            System.out.println(pred.getData());
+        }
+        else{
+            System.out.println("Sorry, that node doesn't exist");
+        }
+    }
+
+    private void maxValue(BinaryNodeInterface<T> node){
+        BinaryNodeInterface<T> current = node;
+
+        while(current.hasRightChild()){
+            current = current.getRightChild();
+        }
+        System.out.println(current.getData());
+    }
+
+    private void minValue(BinaryNodeInterface<T> node){
+        BinaryNodeInterface<T> current = node;
+
+        while(current.hasLeftChild()){
+            current = current.getLeftChild();
+        }
+        System.out.println(current.getData());
     }
 
     public class ReturnObject {
--- Project 1/src/UI.java
+++ Project 1/src/UI.java
@@ -7,12 +7,12 @@ public class UI {
     Scanner keyboard;
     BinarySearchTree<Integer> bst;
 
-    public UI(BinarySearchTree bst){
+    public UI(BinarySearchTree bst) {
         keyboard = new Scanner(System.in);
         this.bst = bst;
     }
 
-    public void welcomeMessage(){
+    public void welcomeMessage() {
         System.out.println("Welcome to the Binary Search" +
                 "Tree project!");
         System.out.println("A binary search tree will be built" +
@@ -21,40 +21,42 @@ public class UI {
 
     }
 
-    private void initialTree(){
+    private void initialTree() {
         System.out.println("Please enter the initial sequence of values: ");
-        while(keyboard.hasNext()){
-            bst.add(keyboard.nextInt());
+        String values = keyboard.nextLine();
+        String[] valueArr = values.split("\\s+");
+
+        for (int i = 0; i < valueArr.length; i++) {
+            bst.add(Integer.parseInt(valueArr[i]));
         }
 
         bst.printPreOrder(bst.getRootNode());
+        System.out.println();
         bst.printInOrder(bst.getRootNode());
+        System.out.println();
         bst.printPostOrder(bst.getRootNode());
+        System.out.println();
     }
 
-    public void promptInput(){
-        System.out.println("I   Insert a value");
-        System.out.println("D   Delete a value");
-        System.out.println("P   Find predecessor");
-        System.out.println("S   Find Successor");
-        System.out.println("E   Exit Program");
-        System.out.println("H   Display this message");
-        System.out.println("Command?");
-
-        String input= "";
-
-        while(!input.equals("E")){
+    public void promptInput() {
+        String input = "";
+        while (!input.equals("E")) {
+            displayPrompt();
             input = keyboard.next().toUpperCase();
-            switch(input) {
+            switch (input) {
                 case "I":
                     bst.add(keyboard.nextInt());
+                    bst.printInOrder(bst.getRootNode());
                     break;
                 case "D":
                     bst.remove(keyboard.nextInt());
+                    bst.printInOrder(bst.getRootNode());
                     break;
                 case "P":
+                    bst.findPredecessor(bst.getRootNode(), keyboard.nextInt());
                     break;
                 case "S":
+                    bst.findSuccessor(bst.getRootNode(), keyboard.nextInt());
                     break;
                 case "E":
                     System.out.println("Thanks for using my program!");
@@ -68,7 +70,15 @@ public class UI {
                     break;
             }
         }
-
     }
 
+    public void displayPrompt() {
+        System.out.println("I   Insert a value");
+        System.out.println("D   Delete a value");
+        System.out.println("P   Find Predecessor");
+        System.out.println("S   Find Successor");
+        System.out.println("E   Exit Program");
+        System.out.println("H   Display this message");
+        System.out.println("Command?");
+    }
 }
